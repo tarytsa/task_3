1e459e6c2452ff39a43699557eed8ff4acdc31f7
tarytsa <a.tarytsa@gmail.com>
2020-06-03 11:38:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExMethod
+{
+    static class DoubleEx
+    {
+        public static double Rational(this int a,int b)
+        {
+            double result = Convert.ToDouble(a)/Convert.ToDouble(b);
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double result = a.Rational(7);
+            Console.WriteLine("{0:f2}",result);
+        }
+    }
+}
